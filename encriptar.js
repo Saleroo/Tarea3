a1da94f17fa3818312d3c21cd8068c2da1160450
Saleroo <50508769+Saleroo@users.noreply.github.com>
2020-10-16 22:06:46 -0300

    Delete encriptar.js

--- encriptar.js
+++
@@ -1,8 +0,0 @@
-import * as myModule from 'jscrypt.js';
-var jscrypto = require('jscrypto');
-
-jscrypto.encryptFile("A.txt","A-encrypted.txt","aes256","P@sW0rD",655000,(progress) =>{
-
-console.log(progress);
-
-});
