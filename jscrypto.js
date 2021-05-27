89cd8b7ad47ae0c801f5911c36bdb1da86d4eac7
Saleroo <50508769+Saleroo@users.noreply.github.com>
2021-05-26 22:52:22 -0400

    Add files via upload

--- jscrypto.js
+++ jscrypto.js
@@ -8,46 +8,10 @@
 // @grant        none
 // @require      https://raw.githubusercontent.com/behdadahmadi/jscrypt/master/jscrypt.js
 //@require       https://raw.githubusercontent.com/nodejs/node/v14.14.0/lib/crypto.js
-'use strict';
-var fs = require('fs');
-var crypto = require('crypto');
 
-function encryptFile(sourcePath, destinationPath, algorithm, password, chunkSize, callback) {
-    chunkSize = (chunkSize === 0) ? chunkSize = 665539 : chunkSize;
-    var inFile = fs.createReadStream(sourcePath, { highWaterMark: chunkSize });
-    var outFile = fs.createWriteStream(destinationPath);
-    var encryptor = crypto.createCipher(algorithm, password);
-    var size = fs.statSync(sourcePath).size;
-    inFile.on('data', function(data) {
-        var percentage = parseInt(inFile.bytesRead) / parseInt(size) * 100;
-        var encrypted = encryptor.update(data);
-        outFile.write(encrypted);
-        callback(percentage);
-    });
-    inFile.on('close', function() {
-        outFile.write(encryptor.final());
-        outFile.close();
-        callback(true);
-    });
-}
+key = "llave secreta de 3DES :o";
 
-function decryptFile(sourcePath, destinationPath, algorithm, password, chunkSize, callback) {
-    chunkSize = (chunkSize === 0) ? chunkSize = 665539 : chunkSize;
-    var inFile = fs.createReadStream(sourcePath, { highWaterMark: chunkSize });
-    var outFile = fs.createWriteStream(destinationPath);
-    var decryptor = crypto.createDecipher(algorithm, password);
-    var size = fs.statSync(sourcePath).size;
-    inFile.on('data', function(data) {
-        var percentage = parseInt(inFile.bytesRead) / parseInt(size) * 100;
-        var decrypted = decryptor.update(data);
-        outFile.write(decrypted);
-        callback(percentage);
-    });
-    inFile.on('close', function() {
-        outFile.write(decryptor.final());
-        outFile.close();
-        callback(true);
-    });
+console.log('decrypt',CryptoJS.TripleDES.decrypt(param, key).toString(CryptoJS.enc.Utf8))
 
-}
-module.exports = { encryptFile, decryptFile }
+
+
